3a5aa84e23934c8bda72ece5a222739f31c6184e
GuilleG02 <guille.g0220rgmail.com>
2025-06-13 19:43:39 +0200

    Configuracion del Proyecto: Node, Nodemon, Express, Mongoose y Docker

---
+++ config/keysExample.js
@@ -0,0 +1,4 @@
+module.exports = {
+ MONGO_URI:
+   'mongodb+srv://USUARIO:CONTRASENA@clusterbbk.5ksywlx.mongodb.net/NOMBRE?retryWrites=true&w=majority&appName=ClusterBBK',
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const { dbConnection } = require('./config/config')
+
+app.use(express.json())
+
+dbConnection()
+
+app.use('/products', require('./routes/products'))
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
