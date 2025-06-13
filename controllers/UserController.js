ca3928ceaee8bfc8d3c92317dc1513ae32d9650b
GuilleG02 <guille.g0220rgmail.com>
2025-06-13 20:43:38 +0200

    Metodo Login junto con Tokens

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,6 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const { jwt_secret } = require('../config/keys.js')
 
 const UserController = {
 
@@ -6,10 +8,31 @@ const UserController = {
    try {
      const user = await User.create(req.body)
      res.status(201).send({ message: 'Usuario registrado con exito', user })
+
+   } catch (error) {
+     console.error(error).send({ message: 'Error al registrar usuario', user })
+   }
+ },
+
+ async login(req, res) {
+
+   try {
+     const user = await User.findOne({
+       email: req.body.email,
+     })
+
+     const token = jwt.sign({ _id: user._id }, jwt_secret)
+
+     if (user.tokens.length > 3) user.tokens.shift()
+     user.tokens.push(token)
+
+     await user.save()
+     res.send({ message: 'Bienvenid@ ' + user.name, token })
+
    } catch (error) {
      console.error(error)
    }
- },
+ }
 
 
 
